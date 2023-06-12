ea070ce0af558a7a40f805ad70e1070598b4434f
Mert Şişmanoğlu <mert190737fb@gmail.com>
2023-06-12 10:39:33 +0300

    chore: Update db config

--- config.js
+++ config.js
@@ -1,16 +1,37 @@
-require('dotenv-safe/config')
-module.exports = {
+require('dotenv-safe').config({
+  allowEmptyValues: true,
+  example: '.env.example'
+})
+
+const configuration = {
   BOT: {
     token: process.env.token,
     slashCommandType: process.env.SLASH_COMMANDS_TYPE
-  },
-
-  DB: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    name: process.env.DB_NAME,
-    connectionLimit: process.env.DB_CONNECTION_LIMIT,
-    connectionIdle: process.env.DB_CONNECTION_IDLE
   }
 }
+
+switch (process.env.DB) {
+  case 'mysql':
+    configuration.DB = {
+      _type: process.env.DB,
+      host: process.env.MYSQL_HOST,
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE
+    }
+    break
+  case 'postgres':
+    configuration.DB = {
+      _type: process.env.DB,
+      connectionString: process.env.POSTGRES_CONNECTION_STRING
+    }
+    break
+  case 'mongodb':
+    configuration.DB = {
+      _type: process.env.DB,
+      connectionString: process.env.MONGODB_CONNECTION_STRING
+    }
+    break
+}
+
+module.exports = configuration
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const { BOT, DB } = require('./config.js')
+const config = require('./config.js')
 const { Client } = require('discord.js')
 const client = new Client({
   intents: 3276799
 })
 
 client
-  .login(BOT.token)
+  .login(config.BOT.token)
   .then(() => console.log('[BOT] Bota giriş yapıldı!'))
   .catch((e) => console.log('[BOT] Bota giriş yapılırken bir hata oluştu:\n' + e))
