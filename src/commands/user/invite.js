5fac8b04408485a099b9b30e8bf012273a45a6d2
Mert Şişmanoğlu <mert190737fb@gmail.com>
2023-06-23 22:27:34 +0300

    feat: Multi language command support

--- src/commands/user/invite.js
+++ src/commands/user/invite.js
@@ -1,19 +1,22 @@
 const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js')
 const metadata = require('../meta/invite.meta')
+const i18n = require('../../i18n')
 
 module.exports = {
   metadata: metadata,
 
   async run(interaction) {
+    let strings = i18n(interaction.guildLocale).cmds.invite
+
     const baseUrl = 'https://discordapp.com/oauth2/authorize'
     let permission = await interaction.options._hoistedOptions[0].value
     let url = `${baseUrl}?client_id=${client.user.id}&scope=bot&permissions=${permission}`
     const button = new ActionRowBuilder().addComponents(
-      new ButtonBuilder().setLabel('Invite me').setURL(url).setStyle(ButtonStyle.Link)
+      new ButtonBuilder().setLabel(strings.inviteLabel).setURL(url).setStyle(ButtonStyle.Link)
     )
 
     await interaction.reply({
-      content: 'Add me to your server!',
+      content: strings.inviteText,
       components: [button],
       ephemeral: true
     })
